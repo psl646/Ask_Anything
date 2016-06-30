409752be4eb09033b7f0fef08d7af90b7b859093
Peter Lin <Peter.Lin646@gmail.com>
2016-06-30 15:30:54 -0400

    anon answer

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -7,6 +7,7 @@ class Api::ResponsesController < ApplicationController
     user_id = params[:response][:user_id]
     answer = Answer.find(answer_id)
     @question = answer.question
+    question_id_symbol = @question.id
 
     if user_id
       response = @question.responses.where(user_id: user_id)
@@ -15,21 +16,23 @@ class Api::ResponsesController < ApplicationController
         Pusher.trigger('question_' + @question.id.to_s, 'response_recorded', {})
         Pusher.trigger('survey_changed', 'response_recorded', {})
       end
-    elsif !session[:anonymous]
+    elsif !session[question_id_symbol]
       Response.create(answer_id: answer_id)
       Pusher.trigger('question_' + @question.id.to_s, 'response_recorded', {})
       Pusher.trigger('survey_changed', 'response_recorded', {})
-      session[:anonymous] = true
+      session[question_id_symbol] = true
     end
     render "api/questions/show"
   end
 
-  # This session[:anonymous] only works for once question.
+  # This session[:anonymous] only works once.
 
   def destroy
     response = Response.find(params[:id])
     if response[:user_id].nil?
-      session[:anonymous] = false
+      @question = response.question
+      question_id_symbol = @question.id
+      session[question_id_symbol] = false
     end
     @question = response.question
     response.destroy
