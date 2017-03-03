696f1dacc2f3bfc44805244b9e1d1d9efd3b49e1
Peter Lin <Peter.Lin646@gmail.com>
2017-03-03 14:47:48 -0500

    Set time zone for delay_job

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module AskAnything
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = Time.zone.name
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -24,6 +24,8 @@ module AskAnything
     config.active_record.raise_in_transactional_callbacks = true
 
     config.active_job.queue_adapter = :delayed_job
+
+
   end
 
 end
