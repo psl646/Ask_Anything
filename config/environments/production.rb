67db5eb0d7dc86e716204117b4ad600aa58c086d
Peter Lin <Peter.Lin646@gmail.com>
2017-03-03 11:50:29 -0500

    Prevent error from fake email addresses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,10 +74,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # Set this to true and configure the email server for
+  # immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
