04a95201713d2bc705d28616110f798b5dbd1eb2
Peter Lin <Peter.Lin646@gmail.com>
2017-03-14 20:41:18 -0400

    Handle incoming sms and create response

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,12 +7,26 @@ class MessagesController < ApplicationController
   def reply
     message_body = params["Body"]
     from_number = params["From"]
+
     boot_twilio
+
+    message_contents = message_body.split
+    question_id = message_contents[0]
+    answer_choice = message_contents[1]
+
+    if (!valid_question = Response.is_valid_question_id?(question_id))
+      return_message = "Invalid question number, try again!"
+    elsif (!current_answer = Response.is_valid_answer_choice?(answer_choice, valid_question))
+      return_message = "Invalid response to question, try again!"
+    else
+      Response.create(answer_id: current_answer.id)
+      return_message = "Your response of: #{answer_choice} to question #{question_id} was recorded! Thanks!"
+    end
 
     sms = @client.messages.create(
       from: ENV['TWILIO_PHONE_NUMBER'],
       to: from_number,
-      body: "Thanks for voting! Your response of: #{message_body} was recorded!"
+      body: return_message
     )
 
     render json: {}
--- app/models/response.rb
+++ app/models/response.rb
@@ -20,4 +20,42 @@ class Response < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  ALPHABETS_HASH = {}
+  ALPHABETS_ARRAY = ("a".."z").to_a
+
+  ALPHABETS_ARRAY.each_with_index do |letter, idx|
+    ALPHABETS_HASH[letter] = idx
+  end
+
+  # a question input is valid if the input is an integer and
+  # if the integer is the id of an active question
+  def is_valid_question_id?(question_input)
+    return false unless is_valid_number?(question_input)
+    current_question = Question.find_by_id(question_input.to_i)
+    return false unless current_question
+    return false if current_question[:active] == false
+    # in order to not have to find the question again
+    # we return the active question instance; the instance will be truthy
+    current_question
+  end
+
+  def is_valid_number?(question_input)
+    question_input.to_i.to_s == question_input
+  end
+
+  def is_valid_answer_choice?(answer_choice, valid_question)
+    letter = answer_choice.downcase
+    return false unless is_valid_letter?(letter)
+    answer_idx = ALPHABETS_HASH[letter]
+    current_answer = valid_question.answers[answer_idx]
+    return false if current_answer.nil?
+    # in order to not have to find the answer again
+    # we return the answer instance; the instance will be truthy
+    current_answer
+  end
+
+  def is_valid_letter?(letter)
+    ALPHABETS_ARRAY.include?(letter)
+  end
 end
